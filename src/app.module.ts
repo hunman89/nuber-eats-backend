4dd01b9db737d8aefe3cf16838094b1face6d1a0
hunman89 <hunman89@gmail.com>
2021-06-19 15:39:51 +0900

    add. connect postgresdb with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurnatsModule } from './restaurnats/restaurnats.module';
 
@@ -9,6 +10,16 @@ import { RestaurnatsModule } from './restaurnats/restaurnats.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     RestaurnatsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'hunman89',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
