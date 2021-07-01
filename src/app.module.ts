21608b010363315868ac53097a76b5ad9780be4c
hunman89 <hunman89@gmail.com>
2021-07-01 21:18:12 +0900

    add. generating jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { UsersModule } from './users/users.module';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        SECRET_KET: Joi.string().required(),
       }),
     }),
     GraphQLModule.forRoot({
