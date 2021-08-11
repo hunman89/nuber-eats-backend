ad6e826007e28b946eb256120ee49b221311cc8e
hunman89 <hunman89@gmail.com>
2021-08-11 21:25:12 +0900

    add. test send verification email

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -35,6 +35,30 @@ describe('MailService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', () => {
+      const sendVerificationEmailArgs = {
+        email: 'email',
+        code: 'code',
+      };
+
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {});
+      service.sendVerificationEmail(
+        sendVerificationEmailArgs.email,
+        sendVerificationEmailArgs.code,
+      );
+
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify Your Email',
+        'confirm',
+        [
+          { key: 'code', value: sendVerificationEmailArgs.code },
+          { key: 'username', value: sendVerificationEmailArgs.email },
+        ],
+      );
+    });
+  });
+
   it.todo('sendEmail');
-  it.todo('sendVerificationEmail');
 });
