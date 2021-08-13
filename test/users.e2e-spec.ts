78611adbe0d21a315908ee14d5a6d52f51d2e715
hunman89 <hunman89@gmail.com>
2021-08-13 19:22:21 +0900

    add. createAccount e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,14 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -17,11 +25,61 @@ describe('UserModule (e2e)', () => {
   });
 
   afterAll(async () => {
-    getConnection().dropDatabase();
+    await getConnection().dropDatabase();
     app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'hunman@gmail.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPQL_ENDPOINT)
+        .send({
+          query: `
+        mutation{
+          createAccount(input:{
+            email:"${EMAIL}"
+            password:"121212"
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exist', () => {
+      return request(app.getHttpServer())
+        .post(GRAPQL_ENDPOINT)
+        .send({
+          query: `
+        mutation{
+          createAccount(input:{
+            email:"${EMAIL}"
+            password:"121212"
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
