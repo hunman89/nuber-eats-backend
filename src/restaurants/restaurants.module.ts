d497923bd5006f4b44e1cd15d4f4fadb4a15df48
hunman89 <hunman89@gmail.com>
2021-08-19 22:27:32 +0900

    add. edit restaurant

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
+import { CategoryRepository } from './repositories/category.repositories';
 import { RestaurnatsResolver } from './restaurants.resolvers';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
+  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
   providers: [RestaurnatsResolver, RestaurantService],
 })
 export class RestaurnatsModule {}
