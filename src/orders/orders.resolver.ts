2a2f4ea1e2b4531336d6ec3b39fae1817ba0b847
hunman89 <hunman89@gmail.com>
2021-08-29 21:17:33 +0900

    add. order resolver, service

---
+++ src/orders/orders.resolver.ts
@@ -0,0 +1,23 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { Order } from './entities/order.entity';
+import { OrderService } from './order.service';
+
+@Resolver((of) => Order)
+@Role(['Client'])
+export class OrderResolver {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Mutation((returns) => CreateOrderOutput)
+  async createOrder(
+    @AuthUser() customer: User,
+    @Args('input') createOrderInput: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    return {
+      ok: true,
+    };
+  }
+}
