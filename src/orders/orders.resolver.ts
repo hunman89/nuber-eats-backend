32a81b567d5928ecf1ce2bba70211f4c99408ec3
hunman89 <hunman89@gmail.com>
2021-09-06 20:15:06 +0900

    add. get token by guard

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -59,7 +59,8 @@ export class OrderResolver {
   }
 
   @Subscription((returns) => String)
-  potatos() {
+  @Role(['Any'])
+  potatos(@AuthUser() user: User) {
     return pubsub.asyncIterator('Potatos');
   }
 }
