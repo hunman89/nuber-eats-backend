df9ec44cb4b048407d45403eae8ab3d346d79482
hunman89 <hunman89@gmail.com>
2021-09-16 22:38:23 +0900

    add. corn job and examples

--- src/payments/payment.service.ts
+++ src/payments/payment.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, Interval, SchedulerRegistry, Timeout } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
@@ -17,6 +18,7 @@ export class PaymentService {
     private readonly payments: Repository<Payment>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    private schedulerRegistry: SchedulerRegistry,
   ) {}
 
   async createPayment(
@@ -68,5 +70,20 @@ export class PaymentService {
         error: 'Could not load patments',
       };
     }
+  }
+
+  @Cron('30 * * * * *', { name: 'myJob' })
+  checkForPayments() {
+    console.log('Checking for payments...(cron)');
+    const job = this.schedulerRegistry.getCronJob('myJob');
+    job.stop();
+  }
+  @Interval(5000)
+  checkForPaymentsi() {
+    console.log('Checking for payments...(interval)');
+  }
+  @Timeout(20000)
+  afterStarts() {
+    console.log('congrats!!');
   }
 }
