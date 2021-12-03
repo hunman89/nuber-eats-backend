2cbdf06fb481f64284457bf80c8d9e75d44b3626
hunman89 <hunman89@gmail.com>
2021-12-03 17:41:54 +0900

    add. payment controller

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { Payment } from './entities/payment.entity';
+import { PaymentsController } from './payment.controller';
 import { PaymentResolver } from './payment.resolver';
 import { PaymentService } from './payment.service';
 
 @Module({
+  controllers: [PaymentsController],
   imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
   providers: [PaymentService, PaymentResolver],
 })
